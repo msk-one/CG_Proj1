4171f134c9e2fde9974a71046993a7b55d53ea9f
Michal Szklarski <msk@nex10.pl>
2016-03-01 00:49:18 +0100

    First things

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,27 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
 
+    public void showAbout(ActionEvent actionEvent) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("About application");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            //((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("MainGUI/ComplexApplication_css.fxml"));
+        primaryStage.setTitle("CG Lab 1 Project");
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
